5ffe59721fc003ad51b9595c240474f27c658f67
jazzDung <dungpham.020901@gmail.com>
2023-05-26 07:59:13 +0700

    test around with sqlalchemy and email

--- financial/jobs.py
+++ financial/jobs.py
@@ -1,7 +1,18 @@
-from dagster import define_asset_job, AssetSelection
+from dagster import define_asset_job, AssetSelection,job, op
 
 refresh_tcbs_job = define_asset_job(
     name="refresh_tcbs",
     selection=AssetSelection.all(),
 )
+
+
+
+def send_email():
+    return True
+
+@job
+def send_email_job():
+    send_email()
+
+
 
